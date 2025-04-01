37f96718d0ce6a5715b726ba4bdf1d5e6f14acfa
sebastianVP <alex.valdezp22@gmail.com>
2025-04-01 10:26:28 -0400

    Programa de descarga

--- DATA/DATA_S4/descargar_DATOS_ANUAL.py
+++ DATA/DATA_S4/descargar_DATOS_ANUAL.py
@@ -6,6 +6,7 @@
 # USAR SERVIDOR PEDIR A GOMERO SI ESTOY DESDE EL ROJ . 
 
 from src.jrodb.jrodb import Api
+import os
 #print("Test de la libreria")
 #print(Api.download.__doc__)
 
@@ -23,18 +24,32 @@ print(months)
 
 # JICAMARCA jic
 # HUANCAYO hyo
-station="tac" #"cuz" #"puc" #"piu"# "hyo" # "jic"
-DIR= "TACNA" # "CUZCO" #"PUCALLPA" #"PIURA" #"HUANCAYO"
+
+# ESTACION S4 ENCONTRADAS DESPUES DE REVISION S4 EN LISN
+# CUZCO,JAEN,JICAMARCA,PIURA,HUANCAYO,SAN_BARTOLOME,PUCP,PUCALLPA
+station="puc"    #"tac" "cuz" #"puc" #"piu"# "hyo" # "jic"
+DIR= "PUCALLPA"  #"TACNA" "CUZCO" #"PUCALLPA" #"PIURA" #"HUANCAYO" "JICAMARCA"
+
+stations=["cuz","jae","jic","piu","hyo","sbr","ucp","puc"]
+DIRS=["CUZCO","JAEN","JICAMARCA","PIURA","HUANCAYO","SAN_BARTOLOME","PUCP","PUCALLPA"]
+#-------------------------------
+# NO ENCUENTRO PARAMETRO S4, TACNA, PUERTO MALDONADO
 # --------------------------------------DESCARGA DATOS ESTACION JICAMARCA----------------------------------------------------
-for year in years:
-    for month in months:
+for station,DIR in zip(stations, DIRS):
+  for year in years:
+      for month in months:
         print("--------------------------------------------------------")
-        print(f"Descargando el mes {month}...")
+        print(f"Descargando el mes {month}...| STATION: {station} | DIR : {DIR}")
         print("--------------------------------------------------------")
+        # Define la ruta donde se guardarán los datos
+        path = f"/home/soporte/Documents/ESTUDIO_CINTILACIONES_PERU/DATA/DATA_S4/{DIR}"
+        # Crea el directorio si no existe
+        os.makedirs(path, exist_ok=True)
+        # Accede a la API y descarga los datos
         with Api('https://lisn.igp.gob.pe/database') as access:
         #with Api('https://www.igp.gob.pe/observatorios/radio-observatorio-jicamarca/database-cielo') a access:
-            id = f'{year}_{month}_scint_data_l{station}'
-            print(access.download(id =id ,path=f"/home/soporte/Documents/ESTUDIO_CINTILACIONES_PERU/DATA/DATA_S4/{DIR}"))
+           id = f'{year}_{month}_scint_data_l{station}'
+           print(access.download(id =id ,path=f"/home/soporte/Documents/ESTUDIO_CINTILACIONES_PERU/DATA/DATA_S4/{DIR}"))
 
 # LUEGO DESCOMPRIMIMOS CON EL COMANDO
 # gzip -d *
