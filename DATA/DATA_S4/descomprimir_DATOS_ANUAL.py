d08e55de8f3e144521a03fa288dc48e9a82d3670
sebastianVP <alex.valdezp22@gmail.com>
2025-04-01 18:50:06 -0400

    descomprimir todos los datos

---
+++ DATA/DATA_S4/descomprimir_DATOS_ANUAL.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+
+def process_directories(input_directories):
+    for input_directory in input_directories:
+        print(f"Procesando directorio: {input_directory}")
+        try:
+            # Cambiar al directorio actual
+            os.chdir(input_directory)
+
+            # Ejecutar el comando gzip -d *
+            print(f"Descomprimiendo archivos en {input_directory}...")
+            subprocess.run(["gzip -d *"], shell=True, check=True)
+
+            print(f"Archivos descomprimidos en {input_directory}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error al ejecutar el comando en {input_directory}: {e}")
+        except Exception as e:
+            print(f"Error al procesar el directorio {input_directory}: {e}")
+
+
+
+# Ruta del directorio de entrada y archivo de salida
+# PATH BASE
+PATH="/home/soporte/Documents/ESTUDIO_CINTILACIONES_PERU/DATA/DATA_S4"
+# DIRECTORIO DE ESTACIONES Y ABREVIATURA
+DIRS=["CUZCO","JAEN","JICAMARCA","PIURA","HUANCAYO","SAN_BARTOLOME","PUCP","PUCALLPA"]
+stations=["cuz","jae","jic","piu","hyo","sbr","ucp","puc"]
+# YEARS
+years= ["2023","2024","2025"]
+# MESES
+months = ["january", "february", "march", "april", "may", "june",
+    "july", "august", "september", "october", "november", "december"]
+
+
+# Lista de directorios a procesar
+input_directories= [os.path.join(PATH,os.path.join(f"2024_{month}_scint_data_ljic","data")) for month in months]
+print("Input_directories: ",input_directories)
+process_directories(input_directories)
